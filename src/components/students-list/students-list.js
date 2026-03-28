3d428f02fbab8662850c089e554b41b65b83fc0b
Dmytro <dimaplyakin2007@gmail.com>
2026-03-28 14:57:44 +0200

    ready ItemsPage

---
+++ src/components/students-list/students-list.js
@@ -0,0 +1,45 @@
+import firstImg from "../../assets/first.jpg";
+import secondImg from "../../assets/second.jpg";
+import thirdImg from "../../assets/third.jpg";
+import fourthImg from "../../assets/fourth.jpg";
+import fifthImg from "../../assets/fifth.jpg";
+
+const students = [
+  {
+    id: 1,
+    name: "Sakura Sato",
+    age: 19,
+    group: "CS-2",
+    photo: firstImg,
+  },
+  {
+    id: 2,
+    name: "Hiroto Tanaka",
+    age: 18,
+    group: "CS-1",
+    photo: secondImg,
+  },
+  {
+    id: 3,
+    name: "Akane Kobayashi",
+    age: 18,
+    group: "CS-1",
+    photo: thirdImg,
+  },
+  {
+    id: 4,
+    name: "Ren Nakamura",
+    age: 20,
+    group: "CS-2",
+    photo: fourthImg,
+  },
+  {
+    id: 5,
+    name: "Hina Yamamoto",
+    age: 19,
+    group: "CS-2",
+    photo: fifthImg,
+  },
+];
+
+export default students;
--- src/pages/ItemsPage/ItemsPage.jsx
+++ src/pages/ItemsPage/ItemsPage.jsx
@@ -1,3 +1,31 @@
-function ItemsPage() {}
+import students from "../../components/students-list/students-list";
+
+import "./itemsPage.scss";
+
+const elements = students.map((item) => {
+  return (
+    <li className="title itemspage__item" key={item.id}>
+      <img src={item.photo} alt={item.name} className="itemspage__img" />
+      <div className="itemspage__descr">
+        <h3 className="title itemspage__name">{item.name}</h3>
+        <div className="title itemspage__age">Вік: {item.age}</div>
+        <div className="title itemspage__group">Група: {item.group}</div>
+      </div>
+    </li>
+  );
+});
+
+function ItemsPage() {
+  return (
+    <section className="itemspage">
+      <div className="container">
+        <h2 className="title itemspage__title">
+          Список <span className="span">банди</span>
+        </h2>
+        <ul className="itemspage__list">{elements}</ul>
+      </div>
+    </section>
+  );
+}
 
 export default ItemsPage;
